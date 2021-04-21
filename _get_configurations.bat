0c5779547a0d22180c79a904b04d6d63f7777219
Daniel <szemiako@users.noreply.github.com>
2021-04-21 12:59:21 -0400

    Updated path

--- _get_configurations.bat
+++ _get_configurations.bat
@@ -1,6 +1,6 @@
 echo off
 set company=%1
 set broker=%2
-sqlcmd -h -1 -W -i "C:\_filemask_query.sql" -s "|" -v Name00=%company% Name01=%broker% -o "_filemask.psv"
+sqlcmd -h -1 -W -i "C:\Scripts\_filemask_query.sql" -s "|" -v Name00=%company% Name01=%broker% -o "_filemask.psv"
 IF ERRORLEVEL 0 echo SqlCmd operations completed
 IF ERRORLEVEL 1 echo SqlCmd operations not completed
